7238c60d2ef04236ecc69bb14bf246ac64ea4878
softpeaks <38219071+softpeaks@users.noreply.github.com>
2022-03-17 18:48:44 +0000

    Update ColourDetection.java

--- ColourDetection.java
+++ ColourDetection.java
@@ -3,6 +3,7 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class ColourDetection {
 	
@@ -11,15 +12,19 @@ public class ColourDetection {
 	static float[] level = new float[colour.sampleSize()];
 	
 	public static void main(String[] args) {
+		LCD.drawString("Colour = ", 2, 2);
 		while(Button.ENTER.isUp()) {
 			colour.fetchSample(level, 0);
-			if(level[0] > level[1] && level[0] > level[2]) {
-				LCD.drawString("Colour = RED", 2, 2);
+			if(level[0] > 0.1 && level[1] > 0.1 && level[2] >0.1) {
+				LCD.drawString("WHITE", 2, 3);
+			}else if(level[0] > level[1] && level[0] > level[2]) {
+				LCD.drawString("RED", 2, 3);
 			}else if(level[1] > level[0] && level[1] > level[2]) {
-				LCD.drawString("Colour = GREEN", 2, 2);
+				LCD.drawString("GREEN", 2, 3);
 			}else if(level[2] > level[0] && level[2] > level[1]) {
-				LCD.drawString("Colour = BLUE", 2, 2);
-			}
+				LCD.drawString("BLUE", 2, 3);
+			}Delay.msDelay(250);
+			LCD.clear(2, 3, 5);
 		}
 	}
 	
